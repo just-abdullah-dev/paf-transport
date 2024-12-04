97bba31da8a209c4caeda374b093e4feb47f5d17
Abdullah <abdulhaseeb9274@gmail.com>
2024-12-05 00:42:35 +0500

    removed hook callls in page.js

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,9 @@
 "use client";
 import DashboardPage from "@/components/DashboardPage/DashboardPage";
 import DashboardSkeleton from "@/components/DashboardPage/DashboardSkeleton";
-import { useAppSelector } from "@/lib/hooks";
 import React from "react";
 
-export default function page() {
-  const user = useAppSelector((state) => state.user);
-  console.log("from redux state dashboard", user);
+export default function Page() {
 
   return (
     <DashboardSkeleton>
