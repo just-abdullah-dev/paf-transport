7bf0faf42302edaea9b232a3ae736a7de1279e6c
Abdullah <abdulhaseeb9274@gmail.com>
2024-12-09 11:15:24 +0500

    student page check again

--- src/components/LandingPage/Layout/Header/Header.jsx
+++ src/components/LandingPage/Layout/Header/Header.jsx
@@ -12,7 +12,7 @@ export default function Header({border=false}) {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
 
   return (
-    <header className={` ${border ? " border-b border-black ":"" } bg-transparent fixed w-full top-0 left-0 z-50 backdrop-blur-lg`}>
+    <header className={` ${border ? " border-b border-black ":"" } fixed w-full top-0 left-0 z-50 backdrop-blur-lg`}>
       <div className="container flex w-full py-4 items-center max-w-7xl px-8 xl:px-0 mx-auto">
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
           <div className="w-full flex-1">
@@ -26,7 +26,7 @@ export default function Header({border=false}) {
           <nav className="flex items-center relative">
             {user?._id ? (
               <div
-                className="flex items-center gap-1 cursor-pointer group"
+                className="flex items-center gap-1 cursor-pointer group bg-custom-gradient"
                 onClick={() => {
                   setIsProfileOpen(!isProfileOpen);
                 }}
@@ -40,13 +40,13 @@ export default function Header({border=false}) {
                 <div
                   className={` ${
                     isProfileOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
-                  } duration-500 transition-all bg-white rounded-lg overflow-hidden backdrop-blur-sm absolute top-[28px] right-0 text-sm`}
+                  } duration-500 transition-all bg-white rounded-lg overflow-hidden backdrop-blur-sm absolute top-[48px] right-0 text-sm`}
                 >
                   <p
                     onClick={() => {
                       window.location.href = "/dashboard";
                     }}
-                    className=" hover:bg-gray-300 duration-300 transition-all cursor-pointer px-3 py-1"
+                    className=" hover:bg-gray-300 duration-300 transition-all cursor-pointer px-4 py-2 text-ternary"
                   >
                     Dashboard
                   </p>
@@ -54,7 +54,7 @@ export default function Header({border=false}) {
                     onClick={() => {
                       dispatch(clearUser());
                     }}
-                    className=" hover:bg-gray-300 duration-300 transition-all cursor-pointer text-red-600 px-3 py-1"
+                    className=" hover:bg-gray-300 duration-300 transition-all cursor-pointer text-red-600 px-4 py-2"
                   >
                     Logout
                   </p>
--- src/models/index.js
+++ src/models/index.js
@@ -6,6 +6,7 @@ import RouteVoucher from "./RouteVoucher";
 import Stop from "./Stop";
 import Student from "./Student";
 import StudentVoucher from "./StudentVoucher";
+import User from "./User";
 
 export default function importModels(){
     console.log("All models created!");
